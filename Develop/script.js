8489ec37f4558ffa54d426d034114d7b55e82259
Zsvob <svobodazachary@gmail.com>
2021-12-16 18:46:43 -0500

    save tasks

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,11 @@
 var ohioTime = document.querySelector("#currentDay");
 var currentTime =moment();
-
 ohioTime.textContent = currentTime.format("MMM DD, YYYY ")
 
+tasks ={}
+
 // change text in timeblock
-$(".container").on("click", ".task", function() {
+$(".container").on("click", "p", function() {
     //get the text
     var text = $(this)
       .text()
@@ -13,18 +14,34 @@ $(".container").on("click", ".task", function() {
     // replace p element with a new textarea
     var textInput = $("<textarea>").addClass("time-block-9 col-10").val(text);
     $(this).replaceWith(textInput);
+    
+    textInput.trigger("focus");
   
-  
-    // // auto focus new element
-    // textInput.trigger("focus");
-  });
+  // console.log(text)
+});
 
+$(".container").on("blur", "textarea", function() {
+  var text = $(this)
+  .val()
+  .trim();
+
+  console.log(text);
+
+  tasks.text = text;
+
+  var taskP = $("<p>")
+  .addClass("task col-10")
+  .text(text);
+
+// replace textarea with p element
+$(this).replaceWith(taskP);
+
+});
 
 
 // Save text in time blocks
   $(".time-block-9").on("click", ".saveBtn", function() {
     //get the textblock text
-   var text9 = $(".time-block-9").find("textarea").text().trim()
-    console.log(text9)
+tasks
   });
 

75a33eeeaa8a95e3be2c76656546d9a6ecbc1231
Zsvob <svobodazachary@gmail.com>
2021-12-17 17:36:23 -0500

    Save time block 9 working

--- Develop/script.js
+++ Develop/script.js
@@ -2,46 +2,81 @@ var ohioTime = document.querySelector("#currentDay");
 var currentTime =moment();
 ohioTime.textContent = currentTime.format("MMM DD, YYYY ")
 
-tasks ={}
+nineAM ={}
+tenAM ={}
+
+hourObject = {
+nine:[],
+ten:[],
+eleven:[],
+twelve: [],
+thirteen: []
+}
+
+var loadTimeblocks = function() {
+  nineAM1 = JSON.parse(localStorage.getItem("nineAM"))
+  if (!nineAM1) {nineAM1 =[]};
+  $("#niner").html(nineAM1.text)
+}
+loadTimeblocks();
 
 // change text in timeblock
-$(".container").on("click", "p", function() {
+$(".time-block-9").on("click", "p", function() {
     //get the text
-    var text = $(this)
-      .text()
-      .trim();
-  
-    // replace p element with a new textarea
+    var text = $(this).text().trim();
     var textInput = $("<textarea>").addClass("time-block-9 col-10").val(text);
-    $(this).replaceWith(textInput);
-    
+    $(this).replaceWith(textInput);    
     textInput.trigger("focus");
-  
-  // console.log(text)
 });
-
-$(".container").on("blur", "textarea", function() {
-  var text = $(this)
-  .val()
-  .trim();
-
-  console.log(text);
-
-  tasks.text = text;
-
-  var taskP = $("<p>")
-  .addClass("task col-10")
-  .text(text);
-
-// replace textarea with p element
+// Send New text to tasks
+$(".time-block-9").on("blur", "textarea", function() {
+  var text = $(this).val().trim();
+  var taskP = $("<p>").addClass("task col-10").text(text);
+  nineAM.text = text;
 $(this).replaceWith(taskP);
-
 });
-
-
 // Save text in time blocks
   $(".time-block-9").on("click", ".saveBtn", function() {
     //get the textblock text
-tasks
+    localStorage.setItem("nineAM", JSON.stringify(nineAM));
   });
 
+
+
+// change text in timeblock
+$(".time-block-10").on("click", "p", function() {
+  //get the text
+  var text = $(this).text().trim();
+  var textInput = $("<textarea>").addClass("time-block-9 col-10").val(text);
+  $(this).replaceWith(textInput);    
+  textInput.trigger("focus");
+});
+
+// Send New text to tasks
+$(".time-block-10").on("blur", "textarea", function() {
+var text = $(this).val().trim();
+var taskP = $("<p>").addClass("task col-10").text(text);
+hourObject.ten.text = text;
+$(this).replaceWith(taskP);
+});
+
+  
+// Save text in time blocks
+$(".time-block-10").on("click", ".saveBtn", function() {
+  //get the textblock text
+ console.log(hourObject.ten)
+});
+
+var timeCheck = function() {
+  
+
+  if (moment().isAfter(9)) {
+    $(taskEl).addClass();
+
+
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+
+};
